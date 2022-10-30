e760ffc1632bd7b28158ac72c0be278693e4c90d
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2022-10-30 12:54:06 +0000

    created order analytics table for tableau

---
+++ models/BI/order_analytics.sql
@@ -0,0 +1,22 @@
+with
+    customers as (select * from {{ ref("dim_customers") }}),
+
+    orders as (select * from {{ ref("fct_orders") }})
+
+select
+    orders.order_id,
+    orders.customer_id,
+    customers.first_name,
+    customers.last_name,
+    orders.order_date,
+    orders.state,
+    orders.payment_id,
+    orders.payment_method,
+    orders.amount,
+    orders.created_at,
+    customers.first_order_date,
+    customers.most_recent_order_date
+
+
+from orders
+left join customers using (customer_id)
