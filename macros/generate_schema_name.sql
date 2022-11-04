4be95d08f1b1872ff4a8ecdf1059f7bf9f9f3f8b
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2022-11-04 09:22:27 +0000

    modified some files

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -2,22 +2,19 @@
     {%- set default_schema = target.schema -%}
     
     {% set log_msg='getting custom schema:\ntarget_name:' ~ target.name ~ '\ncustom_schema_name:' ~ custom_schema_name %}
-    {% do log(log_msg, False) %}
+    {% do log(log_msg, True) %}
 
     {%- if custom_schema_name -%}
         {% if 'default' == target.name %}
+            {{ log("First if: " ~ arg1 ~ ", " ~ arg2) }}
             {{default_schema}}
-        {% elif 'merge_request' in target.name.lower() -%}
+        {% elif 'ci' in target.name.lower() -%}
             {{target.schema}}{{ '_' ~ custom_schema_name if custom_schema_name else ''}}
         {% else %}
             {{custom_schema_name}}
         {% endif %}
     {%- else -%} 
-        {% if node.path != 'rpc/request' or node.path != None %}
-            {% set log_msg='Invalid Schema Config for model '~ node.path %}
-            {% do log(log_msg, True) %}
-        {%- else -%}
-            {{ exceptions.raise_compiler_error("Invalid Schema Config for model "~ node.path ~ '\n Please check the dbt_project.yml to make sure the schema config is defined.') }} 
-        {%- endif -%}
+        {{ log("Last Else: ") }}
+        {{ default_schema }}
     {% endif %}
 {%- endmacro %}	
